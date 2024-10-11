d87cde445f0adc7b6b8f619a7ceecf6b913dc5ff
SnehaAkkenapally <sneha.akkenapally11@gmail.com>
2024-10-11 11:31:40 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public static int[] GenerateNumbers(int length)
+        {
+            int[] numbers = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            return numbers;
+        }
+
+        public static void Reverse(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[length - i - 1];
+                array[length - i - 1] = temp;
+            }
+        }
+
+        public static void PrintNumbers(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        public static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static void PrintFibonacci(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write(Fibonacci(i) + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+Class1 class1  = new Class1();
+
+int[] numbers = Class1.GenerateNumbers(10); // Generate an array of length 10
+Class1.Reverse(numbers); // Reverse the array
+Class1.PrintNumbers(numbers);
+
+
+Class1.PrintFibonacci(10);
